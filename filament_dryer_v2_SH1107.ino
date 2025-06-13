6ff81fbdba45a68b360aca345950dc33775c8f4a
Matthew Greathouse <132587783+c0deirl@users.noreply.github.com>
2025-06-13 08:24:03 -0400

    Wifi Credentials change

--- filament_dryer_v2_SH1107.ino
+++ filament_dryer_v2_SH1107.ino
@@ -7,8 +7,8 @@
 #include <PubSubClient.h> // MQTT Library										 
 
 // Replace with your network credentials
-const char* ssid = "N8MDG";
-const char* password = "mattg123";
+const char* ssid = "SSID HERE";
+const char* password = "PASSWORD HERE";
 
 // MQTT broker settings
 const char* mqtt_server = "MQTT_BROKER_IP";    // e.g., "192.168.1.10" or "broker.hivemq.com"
